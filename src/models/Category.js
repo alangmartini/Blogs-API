06fa341535de7dc9633142ad9bc4e616a7eaddfd
gmaralan <gmartinialan@gmail.com>
2023-04-06 06:24:39 -0400

    fix: fixed an error on posts-categories where it had its on id, instead of just being a junction table

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+Category.belongsToMany(require('./BlogPost'), { through: 'posts_categories' });
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'categories',
+  });
+
+  Category.associate = (models) => {
+    Category.belongsToMany(models.BlogPost, { through: 'posts_categories' });
+  };
+  
+  return Category;
+};
