4a10a6029de6c003b7cc27769acc74c96faa05dc
gmaralan <gmartinialan@gmail.com>
2023-04-06 13:54:42 -0400

    fix: fixed req 01

--- src/migrations/4-posts_categories.js
+++ src/migrations/4-posts_categories.js
@@ -1,28 +1,31 @@
 /* eslint-disable max-lines-per-function */
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => queryInterface.createTable('posts_categories', {
-    postId: {
-      type: Sequelize.INTEGER,
-      field: 'post_id',
-      references: {
-        model: 'blog_posts',
-        key: 'id',
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       },
-    },
-    categoryId: {
-      type: Sequelize.INTEGER,
-      field: 'category_id',
-      references: {
-        model: 'categories',
-        key: 'id',
+      categoryId: {
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       },
-    },
-    onUpdate: 'CASCADE',
-    onDelete: 'CASCADE',
-
-  }),
-
+      
+    });
+  },
   down: async (queryInterface, _Sequelize) => {
     queryInterface.dropTable('posts_categories');
   },
