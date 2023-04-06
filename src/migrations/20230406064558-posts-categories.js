ed74457d5ed750f15bfab3cebd20ee3d00e42226
gmaralan <gmartinialan@gmail.com>
2023-04-06 03:16:17 -0400

    chore: created PostsCategories migration

--- src/migrations/20230406064558-posts-categories.js
+++ src/migrations/20230406064558-posts-categories.js
@@ -1,21 +1,32 @@
-'use strict';
+/* eslint-disable max-lines-per-function */
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+  up: async (queryInterface, Sequelize) => queryInterface.createTable('PostsCategories', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    postIdd: {
+      type: Sequelize.INTEGER,
+      field: 'post_id',
+      references: {
+        model: 'blog_posts',
+        key: 'id',
+      },
+    },
+    categoryId: {
+      type: Sequelize.INTEGER,
+      field: 'category_id',
+      references: {
+        model: 'categories',
+        key: 'id',
+      },
+    },
+  }),
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    queryInterface.dropTable('PostsCategories');
+  },
 };
