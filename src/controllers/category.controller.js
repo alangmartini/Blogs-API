b154db911b9316549674d9ae4486ed8a90648031
gmaralan <gmartinialan@gmail.com>
2023-04-06 08:39:55 -0400

    chore: finished category register endpoint

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -2,15 +2,16 @@ const { statusCode } = require('../errors/errors.error');
 const categoryService = require('../services');
 
 const registerCategory = async (req, res) => {
-    try {
-      const { name } = req.body;
+  const { name } = req.body;
 
-      const newCategory = await categoryService.createCategory(name);
+  const newCategory = await categoryService.createCategory(name);
 
-      res.status(statusCode.SUCCESFULLY_CREATED).json(newCategory);
-    } catch (err) {
-      res.status(statusCode.INTERNAL_ERROR).json({ message: err.message });
-    }
+  if (newCategory.statusCode) {
+    const error = newCategory;
+    res.status(error.statusCode).json({ message: error.message });
+  }
+
+  res.status(statusCode.SUCCESFULLY_CREATED).json(newCategory);
   };
 
 module.exports = {
