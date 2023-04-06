c963e59352d4d6b4c20b62f9c3a2c6d82c576a33
gmaralan <gmartinialan@gmail.com>
2023-04-06 14:56:50 -0400

    fix: req 11

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,14 +1,43 @@
 /* eslint-disable max-lines-per-function */
-module.exports = (sequelize, _DataTypes) => {
-  const PostCategory = sequelize.define('PostCategory', {});
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'post_id',
+      references: {
+        model: 'BlogPost',
+        key: 'id',
+      },
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'category_id',
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+    },
+  }, {
+    tableName: 'posts_categories',
+    timestamps: false,
+    underscored: true,
+  });
   
   PostCategory.associate = (models) => {
     models.BlogPost.belongsToMany(models.Category, { 
-      through: 'PostCategory', 
+        as: 'categories',
+        through: 'PostCategory', 
+        foreignKey: 'postId',
+        otherKey: 'categoryId',
       });
 
     models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
       through: 'PostCategory', 
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
     });
 };
 
