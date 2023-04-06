596bcee6473469f5a6779241c8521e23fdf324a4
gmaralan <gmartinialan@gmail.com>
2023-04-06 07:12:46 -0400

    chore: finished category validation middleware

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
 const categoryService = require('../services');
 
 const registerCategory = async (req, res) => {
     try {
       const { name } = req.body;
+
       const newCategory = await categoryService.createCategory(name);
+
       res.status(201).json(newCategory);
     } catch (err) {
       res.status(500).json({ message: err.message });
--- src/middleware/categoryName.middleware.js
+++ src/middleware/categoryName.middleware.js
@@ -1,12 +1,13 @@
+const { statusCode } = require('../errors/errors.error');
 const { registerUserSchema } = require('./validation/schema');
 
 const categoryNameMiddleware = (req, res, next) => {
   const { error } = registerUserSchema.validate(req.body);
 
   if (error) {
-    return res.status(400).json({ error: error.details[0].message });
+    return res.status(statusCode.INVALID_REQUEST).json({ error: error.details[0].message });
   }
-  
+
   next();
 };
 
