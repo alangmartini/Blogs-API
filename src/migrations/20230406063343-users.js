9c8010bd727da464769b35e1da51bb7bf4ea96b1
gmaralan <gmartinialan@gmail.com>
2023-04-06 03:38:43 -0400

    fix: added image props to user migration

--- src/migrations/20230406063343-users.js
+++ src/migrations/20230406063343-users.js
@@ -24,13 +24,9 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
       },
     });
       },
